ecca4becdc9e3906838c924860e44752ba25c10c
Speeddy1984 <speeddy1984@yandex.ru>
2024-05-13 22:37:17 +0800

    async/await hw added ci and fixed unit test

--- src/js/__tests__/apptest.js
+++ src/js/__tests__/apptest.js
@@ -3,12 +3,16 @@ import GameSavingLoader from '../gamesavingloader'
 test('GameSavingLoader.load() returns correct data', async () => {
     const result = await GameSavingLoader.load();
   
-    expect(result.id).toBe(9);
-    expect(result.created).toBe(1546300800);
-    expect(result.userInfo.id).toBe(1);
-    expect(result.userInfo.name).toBe('Hitman');
-    expect(result.userInfo.level).toBe(10);
-    expect(result.userInfo.points).toBe(2000);
+    expect(result).toEqual({
+      id: 9,
+      created: 1546300800,
+      userInfo: {
+        id: 1,
+        name: "Hitman",
+        level: 10,
+        points: 2000,
+      },
+    });
   });
 
   test('GameSavingLoader.load() returns error', async () => {
